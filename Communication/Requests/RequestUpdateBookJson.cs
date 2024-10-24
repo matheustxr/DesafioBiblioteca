0a1ea4b1d7dd946f40d895fc03ea3071785b6717
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-10-24 20:26:55 -0300

    create route put and RequestUpdateBookJson

---
+++ Communication/Requests/RequestUpdateBookJson.cs
@@ -0,0 +1,12 @@
+﻿namespace DesafioBiblioteca.Communication.Requests;
+
+public class RequestUpdateBookJson
+{
+    public string Title { get; set; } = string.Empty;
+
+    public string Gender { get; set; } = string.Empty;
+
+    public double Price { get; set; } = 0.0;
+
+    public int Stock { get; set; } = 1;
+}
--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -11,14 +11,17 @@ public class BookController : ControllerBase
     [HttpGet]
     [Route("{id}")]
     [ProducesResponseType(typeof(Book), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
     public IActionResult GetById ([FromRoute]string id)
     {
         return Ok();
     }
 
+
     [HttpPost]
     [ProducesResponseType(typeof(RequestCreateBookJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
     public IActionResult CreateBook ([FromBody] RequestCreateBookJson request)
     {
         var response = new RequestCreateBookJson
@@ -33,5 +36,11 @@ public class BookController : ControllerBase
     }
 
 
-
+    [HttpPut]
+    [ProducesResponseType(typeof(RequestUpdateBookJson), StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof (string), StatusCodes.Status400BadRequest)]
+    public IActionResult Update([FromBody] RequestUpdateBookJson request)
+    {
+        return NoContent();
+    }
 }
